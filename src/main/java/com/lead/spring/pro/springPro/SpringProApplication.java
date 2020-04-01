7c54b3388aa1fc6d7a3bc09e47fd80ae88714000
Oleksandr Zhdan <oleksandr.zhdan@synapse.com>
2020-04-01 19:31:08 +0300

    no message

---
+++ src/main/java/com/lead/spring/pro/springPro/SpringProApplication.java
@@ -0,0 +1,13 @@
+package com.lead.spring.pro.springPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProApplication.class, args);
+	}
+
+}
